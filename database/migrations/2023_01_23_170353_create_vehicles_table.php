fe11cb82621d96651e81b6792be176607362c1ee
Cristina <cpq002@alumnos.ucn.cl>
2023-01-23 14:34:49 -0300

    feat(Vehicle): Se agrega modelo, migracion y controlador

---
+++ database/migrations/2023_01_23_170353_create_vehicles_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use App\Models\Person;
+use App\Models\Vehicle;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vehicles', function (Blueprint $table) {
+            $table->id();
+            $table->string('marca');
+            $table->string('modelo');
+            $table->integer('anio');
+            $table->integer('precio');
+
+            $table->foreignIdFor(Person::class)->constrained();
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vehicles');
+    }
+};
